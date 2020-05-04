ec459c99599e47956231da86c628b9693e600a98
Johanna Vuorinen <johanna.vuorinen@edu.bc.fi>
2020-05-04 20:24:42 +0300

    buildui

--- index.js
+++ index.js
@@ -97,7 +97,6 @@ const generateID = () => {
 }
 
 const PORT = process.env.PORT || 3001;
-console.log(morgan('tiny'));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
